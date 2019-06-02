3380375c7c44d82b0baab912135b5bae49167e1d
kingyz <761294412@qq.com>
2019-06-02 18:08:25 +0800

    Scroll Intercept

---
+++ scrollinterceptdemo/src/main/java/com/kingyzll/scrollinterceptdemo/view/CustomScrollView.java
@@ -0,0 +1,61 @@
+package com.kingyzll.scrollinterceptdemo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+import android.widget.Scroller;
+
+public class CustomScrollView extends ScrollView {
+    Scroller mScroller;
+    int mLastXIntercept;
+    int mLastYIntercept;
+
+    public CustomScrollView(Context context) {
+        super(context);
+    }
+
+    public CustomScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onInterceptHoverEvent(MotionEvent event) {
+        boolean intercepted = false;
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                intercepted = false;
+                if (!mScroller.isFinished()) {
+                    mScroller.abortAnimation();
+                    intercepted = true;
+                }
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                int deltaX = x - mLastXIntercept;
+                int deltaY = y - mLastYIntercept;
+                if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                    intercepted = true;
+                } else {
+                    intercepted = false;
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP:{
+                intercepted = false;
+                break;
+            }
+            default:
+                break;
+        }
+        mLastXIntercept = x;
+        mLastYIntercept = y;
+        return intercepted;
+    }
+}
